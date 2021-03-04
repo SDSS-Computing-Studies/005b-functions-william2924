7917af812ca2563810f9d640e185695afe0e83c7
william2924 <78515221+william2924@users.noreply.github.com>
2021-03-03 23:22:35 -0800

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -8,10 +8,30 @@ into another number.
 Example: The factors of 10 are 1, 2, 5, 10
 (2 points)
 """
-def factors(number):
-    list = []
-    print("factors ("+ str(number) +") == ",end = "")
+
+
+def factor(number):
+    mylist = []
+    number = 37
+    print(" The factors of " + str(number) + " are ", end=" ")
     for whole_number in range(1,number + 1):
         if number % whole_number == 0:
-           list.append(whole_number)
-    print(list)
+            mylist.append(whole_number)
+            print(mylist)
+
+
+def factors(x):
+   print("The factors of",x,"are:",end=" ")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           mylist.append(i)
+           print(mylist)
+           
+
+mylist = []
+
+num = 12
+factors(num)
+
+num = 37
+factors(num)
